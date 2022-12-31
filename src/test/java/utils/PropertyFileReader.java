8c1af7d73c289d7518610b6d18364b9a2689999e
AboubakarAhamada <aboumarshal@hotmail.fr>
2022-12-31 19:36:59 +0100

    Link where I picked up readProperty method

--- src/test/java/utils/PropertyFileReader.java
+++ src/test/java/utils/PropertyFileReader.java
@@ -8,6 +8,8 @@ import java.util.Properties;
 // This class contains a method to read the browser type specified in /environment/config.properties file
 public class PropertyFileReader {
 
+    // Yes, I stole this on from here https://www.tutorialspoint.com/how-to-read-the-data-from-a-properties-file-in-java
+    // But not because I'm Black (Will Smith)
     public Properties readProperty(String filePath) throws IOException {
         FileInputStream fis = null;
         Properties prop = null;
