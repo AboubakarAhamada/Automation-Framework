53c229818a05cf9b253f29deab6b23645884e4a9
AboubakarAhamada <aboumarshal@hotmail.fr>
2023-01-17 21:54:17 +0100

    Changer @Before to @beforeAll and @after to @AfterAll

--- src/test/java/modules/Hooks.java
+++ src/test/java/modules/Hooks.java
@@ -1,25 +1,32 @@
 package modules;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
+import io.cucumber.java.BeforeAll;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import page_objects.GitHubSearchPage;
+import page_objects.GitHubHomePage;
+import page_objects.LoginPage;
 import utils.BrowserFactory;
 
 public class Hooks {
     //TODO: Configure test run before and after
     public static WebDriver driver;
-    GitHubSearchPage gitHubSearchPage = new GitHubSearchPage();
+    private static GitHubHomePage gitHubHomePage = new GitHubHomePage();
+    private static LoginPage loginPage = new LoginPage();
 
-    @Before
-    public void setUp(){
+    @BeforeAll
+    public static void beforeAll(){
         driver = BrowserFactory.getFactoryDriver();
-        PageFactory.initElements(driver, gitHubSearchPage);
+        PageFactory.initElements(driver, gitHubHomePage);
+        PageFactory.initElements(driver, loginPage);
+
+
     }
 
-    @After
-    public  void tearDown(){
+    @AfterAll
+    public  static void afterAll(){
         driver.quit();
     }
 }
